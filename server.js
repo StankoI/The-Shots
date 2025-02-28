f09c668513bea4b1c3dc1f2be7d0eafbbd7d02b5
Stanislav <stanislavigov4@gmail.com>
2025-02-28 17:44:54 +0200

    project setup

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const port = 3000
+
+
+app.get('/', (req,res) => {
+    res.send("hello the shots");
+})
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`);
+})
