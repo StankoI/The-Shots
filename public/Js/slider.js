aed33b9f017501cea964aa3b13a6f4e1a8cd1aed
Stanislav <stanislavigov4@gmail.com>
2025-03-08 20:27:47 +0200

    some small changes

--- public/Js/slider.js
+++ public/Js/slider.js
@@ -1,5 +1,4 @@
 
-
 class Slider{
     
     constructor(){
@@ -57,54 +56,3 @@ class Slider{
 }
 
 new Slider();
-
-class DropDown {
-
-    constructor() {
-        this.active = true;
-        this.btn = document.getElementById("drop");
-        this.daysOfWeek = document.querySelectorAll('.date');
-        this.workTimeDays = document.querySelectorAll('.dayTime');
-        this.toDayWorkTime = document.getElementById('show');
-        this.workTimeTable = document.getElementById('table');
-        this.init();
-        this.setData();
-    }
-
-    init() {
-        this.btn.addEventListener('click', () => { this.showWorkTimeTable() });
-    }
-
-    setData() {
-        this.daysOfWeek.forEach((day) => { day.classList.remove("currentDate") });
-        let todayDate = new Date();
-
-        let today = todayDate.getDay();
-
-        if (today === 0) {
-            today = 6;
-        }
-        else {
-            today--;
-        }
-        this.daysOfWeek[today].classList.add("currentDate");
-
-        this.workTimeDays.forEach((day) => { day.classList.remove("cur") });
-        this.workTimeDays[today].classList.add("cur");
-    }
-
-    showWorkTimeTable() {
-        if (this.active) {
-            this.workTimeTable.style.transform = "scaleY(1)";
-            this.btn.style.transform = "rotateX(180deg)";
-            this.active = false;
-        }
-        else {
-            this.workTimeTable.style.transform = "scaleY(0)";
-            this.btn.style.transform = "rotateX(0deg)";
-            this.active = true;
-        }
-    }
-}
-
-new DropDown();
