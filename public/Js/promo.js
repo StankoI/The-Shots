39de134e95fab08b4c85389d53921a31076d67db
Stanislav <stanislavigov4@gmail.com>
2025-04-22 15:45:34 +0300

    promo.js added

--- public/Js/promo.js
+++ public/Js/promo.js
@@ -1,13 +1,45 @@
 
-export class Promo{
+export class Promo {
 
-    constructor(Id)
-    {
+    constructor(Id) {
         this.element = document.getAnimations(Id);
     }
 
-    generateElement(data){
-        
-    }
+    generatePage(data) {
+        data.forEach((elem) => {
+            const Div = document.createElement('div');
+            Div.classList.add('element');
 
+            const name = document.createElement('div');
+            name.classList.add('name');
+            name.appendChild(document.createTextNode(elem.name));
+
+            const age = document.createElement('div');
+            age.classList.add('price');
+            age.appendChild(document.createTextNode(elem.price));
+
+            const img = document.createElement('div');
+            img.classList.add('img');
+
+            img.style.backgroundImage = `url(${elem.img})`;
+
+            const ingredients = document.createElement('div');
+            ingredients.classList.add("ingredients");
+
+            elem.ingredients.forEach((ingr) => {
+
+                const tmp = document.createElement("h4");
+                tmp.textContent = ingr;
+
+                ingredients.appendChild(tmp);
+            })
+
+            Div.appendChild(ingredients);
+            Div.appendChild(name);
+            Div.appendChild(age);
+            Div.appendChild(img);
+
+            container.appendChild(Div);
+        })
+    }
 }
