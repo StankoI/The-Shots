4af6e3211bddc30196a0a2ffe3c9e15e20b6697b
Stanislav <stanislavigov4@gmail.com>
2025-04-23 14:05:52 +0300

    finished the page

---
+++ public/Js/promo.js
@@ -0,0 +1,37 @@
+
+export class Promo {
+
+    constructor(Id) {
+        this.container = document.getElementById(Id);
+    }
+
+    generatePage(data) {
+        data.forEach((elem) => {
+            const Div = document.createElement('div');
+            Div.classList.add('element');
+
+            const name = document.createElement('div');
+            name.classList.add('name');
+            name.appendChild(document.createTextNode(elem.name));
+
+            const age = document.createElement('div');
+            age.classList.add('price');
+            age.appendChild(document.createTextNode(elem.price));
+
+            const img = document.createElement('div');
+            img.classList.add('img');
+            img.style.backgroundImage = `url(${elem.img})`;
+
+            const description = document.createElement('div');
+            description.classList.add("description");
+            description.appendChild(document.createTextNode(elem.description));
+
+            Div.appendChild(description);
+            Div.appendChild(name);
+            Div.appendChild(age);
+            Div.appendChild(img);
+
+            this.container.appendChild(Div);
+        })
+    }
+}
